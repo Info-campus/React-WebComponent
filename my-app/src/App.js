d4c6cba85518833999bea57178ce4b231c371dcf
karthik <karthhikmn156@gmail.com>
2020-02-23 11:23:28 +0530

    23/02/2020 changed name to Karthik

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from  './Component/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>This is Karthik S M</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <Header></Header>
       </header>
       
     </div>
