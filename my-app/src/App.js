721ba6cb9867b681ed05f07cb91ffd06205e7b5c
Dora <ramanna.dora@accenture.com>
2020-02-23 11:23:24 +0530

    1/23/2020-Dora

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>This is Dora</div>
         <img src={logo} className="App-logo" alt="logo" />
         <Header></Header>
       </header>
