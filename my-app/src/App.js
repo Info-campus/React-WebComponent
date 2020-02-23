05301f2f43514c3f0bafce1c7c9db8d3f3d54431
pvenkatesh56 <pvenkateswarlu56@gmail.com>
2020-02-23 11:23:16 +0530

    1/23/2020_changed named to venkateh

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from  './Component/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>this is venkateswarlu</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <Header></Header>
+      
       </header>
       
     </div>
