4868ae831354f7a136ff39a0c6e314c938923a3f
rashmiarun918 <rashmiarun918@gmail.com>
2020-02-23 11:23:12 +0530

    2/23/2020-changed name to Rashmi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from  './Component/Header/Header'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>This is Rashmi</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <Header></Header>
+        
       </header>
       
     </div>
