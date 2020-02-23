5b1dec5de4ecf77254023155eeaa6fdbf62fd673
akshaypal007 <akshaypal.ak007@gmail.com>
2020-02-23 11:23:26 +0530

    2/23/2020-changed name to akshay

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from  './Component/Header/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>This is akshay</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <Header></Header>
       </header>
       
     </div>
